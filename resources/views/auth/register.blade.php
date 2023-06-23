3082132e99d20cc84d71eaa9b755bd47ab51629f
josearcos2023 <jose.arcos@tecsup.edu.pe>
2023-06-23 16:33:22 -0500

    Primer commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.guest')
+@section('content')
+<body class="fondo">
+    <div style="display: flex;flex-direction: column;">
+        <div class=" nav_homesite"><!--Sección superior de navegación y logo-->
+            <div class=""> <!--Icono del logo de la aplicación-->
+                <p style="color: aliceblue;">Preguntados</p>
+            </div>
+            <div class="nav_list"><!--Opciones de navegación de inicio-->
+                <div class="box_link">
+                    <a href="" class="nav_link">
+                        <p class="nav_text">Mas info</p>
+                    </a>
+                </div>
+                <div class="box_link">
+                    <a href="" class="nav_link">
+                        <p class="nav_text">Cómo jugar?</p>
+                    </a>
+                </div>
+                <div class="box_link">
+                    <a href="{{ route('login') }}" class="nav_link">
+                        <p class="nav_text">Iniciar sesión</p>
+                    </a>
+                </div>
+                <div class="box_link">
+                    <a href="#" class="nav_link">
+                        <p class="nav_text">Registrarse</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    {{-- <authentication-card>
+        <slot name="logo">
+            <authentication-card-logo>
+        </slot>
+        <validation-errors class="mb-4"> --}}
+<div class="d-flex justify-content-center align-items-center"><!--sección principal-->
+    <div class="d-flex" style=" background-color: rgb(255, 255, 255);">
+        <div class="col-lg-6  col-12 "><!--sección registro-->
+            <div style="padding: 50px;">
+                <div>
+                    <p style="font-size: 40px;padding: 0px 0px 0px 20px;">Registro</p>
+                </div>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="double_col box_input">
+                <label for="name" value="Name">Nombre</label>
+                <input id="name" class="registro_input" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            </div>
+
+            <div class="double_col box_input">
+                <label for="lastname" value="Lastname">Apellido</label>
+                <input id="lastname" class="registro_input" type="text" name="lastname" :value="old('lastname')"/>
+            </div>
+
+            <div class="double_col box_input">
+                <label for="email" value="Email">Correo</label>
+                <input id="email" class="registro_input" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            </div>
+
+            <div class="double_col box_input">
+                <label for="id_especialidades" value="id_especialidades">Especialidad</label>
+                {{-- <input id="especialidad" class="registro_input" type="text" name="especialidad" :value="old('id_especialidad')"/> --}}
+                <select class="form-select" aria-label="Default select example" name="id_especialidades">
+                    <option selected>Select</option>
+                    <option value="1">Tecnología Digital</option>
+                    <option value="2">Mecatrónica</option>
+                    <option value="3">Gestión y Producción</option>
+                  </select>
+            </div>
+
+            <div class="single_col box_input">
+                <label for="password" value="Password">Contraseña</label>
+                <input id="password" class="registro_input" type="password" name="password" required autocomplete="new-password" />
+            </div>
+
+            <div class="single_col box_input">
+                <label for="password_confirmation" value="Confirm password">Confirmar contraseña</label>
+                <input id="password_confirmation" class="registro_input" type="password" name="password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="mt-4">
+                    <label for="terms">
+                        <div class="flex items-center">
+                            <checkbox name="terms" id="terms" required />
+
+                            <div class="ml-2">
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">'.__('Terms of Service').'</a>',
+                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">'.__('Privacy Policy').'</a>',
+                                ]) !!}
+                            </div>
+                        </div>
+                    </label>
+                </div>
+            @endif
+                <br>
+            <div>
+                <div class="d-flex justify-content-center" style="padding-top: 10px;">
+                    <button class="ml-4 btn btn-outline-secondary btn-lg" style="color: rgb(0, 0, 0)">
+                        {{ __('Registrarme') }}
+                    </button>
+                </div>
+                <div class="d-flex justify-content-center" style="padding-top: 50px;">
+                    <p>¿Tienes una cuenta?</p>
+                    <a href="{{ route('login') }}" class="d-flex " style="display: flex;">
+                    {{ __('Inicia sesión') }}
+                    </a>
+                </div>
+                
+            </div>
+        </form>
+{{-- Inicio Texto extra --}}
+                
+{{-- Fin texto extra --}}
+            </div>
+        </div>
+        <div class="col-lg-6 card box_main_img d-flex justify-content-center align-items-center box_main_form" style="background-color: aqua;"> <!--seccion imagen-->
+            <img src="/images/nave_espacial_login.jpg" alt="dibujo digital de nave espacial" width="100%" height="100%">
+            <div class="card-img-overlay d-flex align-items-start flex-column bd-highlight mb-3 ">
+                <div class="p-2  d-flex flex-column justify-content-center" style="width: 100%; height: 50%;">
+                    <!--Texto superiore-->
+                    <div class="d-flex align-items-center justify-content-center" style="height: 50%;">
+                        <p class="text-white " style="font-size: 35px;text-align: center;">Estás a punto de conocer SPACES QUIZ</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+
+
+
+
+    </div>
+</div>
+    {{-- </authentication-card> --}}
+@stop
+</body>
